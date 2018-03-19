56324884caafe5d5bc2da1eea2a1e30a0b79c283
ethanow <owch0002@e.ntu.edu.sg>
2018-03-20 00:46:34 +0800

    Revert "activating dialogflow"
    This reverts commit 1c9e8eb2cbd241565568b04deae6ef1abd2e8928.

--- index.js
+++ index.js
@@ -1,104 +1,43 @@
+'use strict';
+
 
 const apiai = require('apiai');
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const Telegram = require('telegram-node-bot'),
 
-const TelegramBot = require('./src/telegrambot');
-const TelegramBotConfig = require('./src/telegrambotconfig');
 
-const REST_PORT = (process.env.PORT || 5000);
-const DEV_CONFIG = process.env.DEVELOPMENT_CONFIG == 'true';
 
-const APP_NAME = process.env.APP_NAME;
-const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
-const APIAI_LANG = process.env.APIAI_LANG;
-const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+PersistentMemoryStorage = require('./adapters/PersistentMemoryStorage'),
+storage = new PersistentMemoryStorage(
+    `${__dirname}/data/userStorage.json`,
+    `${__dirname}/data/chatStorage.json`
+),
 
-var baseUrl = "";
-if (APP_NAME) {
-    // Heroku case
-    baseUrl = `https://${APP_NAME}.herokuapp.com`;
-} else {
-    console.error('Set up the url of your service here and remove exit code!');
-    process.exit(1);
+tg = new Telegram.Telegram(process.env.TELEGRAM_TOKEN, {
+    workers: 1,
+    storage: storage
+});
+
+// const gsheet = require('./controllers/gsheet')
+
+// console.log(new Telegram.TextCommand);
+const AddCtrl = require('./controllers/add')
+    , OtherwiseController = require('./controllers/otherwise');
+
+const addcontrol = new AddCtrl();
+
+
+// Defining Telegram commands
+tg.router.when(new Telegram.TextCommand('/add', 'addCommand'), addcontrol)
+.when(new Telegram.TextCommand('/total', 'totalCommand'), addcontrol)
+.otherwise(new OtherwiseController());
+
+function exitHandler(exitCode) {
+    storage.flush();
+    process.exit(exitCode);
 }
 
-// console timestamps
-require('console-stamp')(console, 'yyyy.mm.dd HH:MM:ss.l');
-
-const botConfig = new TelegramBotConfig(
-    APIAI_ACCESS_TOKEN,
-    APIAI_LANG,
-    TELEGRAM_TOKEN);
-
-botConfig.devConfig = DEV_CONFIG;
-
-const bot = new TelegramBot(botConfig, baseUrl);
-bot.start(() => {
-        console.log("Bot started");
-    },
-    (errStatus) => {
-        console.error('It seems the TELEGRAM_TOKEN is wrong! Please fix it.')
-    });
-
-
-const app = express();
-app.use(bodyParser.json());
-
-app.post('/webhook', (req, res) => {
-    console.log('POST webhook');
-
-    try {
-        bot.processMessage(req, res);
-    } catch (err) {
-        return res.status(400).send('Error while processing ' + err.message);
-    }
-});
-
-app.listen(REST_PORT, function () {
-    console.log('Rest service ready on port ' + REST_PORT);
-});
-
-// 'use strict';
-
-
-// const apiai = require('apiai');
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const dotenv = require('dotenv').config();
-// const Telegram = require('telegram-node-bot'),
-
-
-
-// PersistentMemoryStorage = require('./adapters/PersistentMemoryStorage'),
-// storage = new PersistentMemoryStorage(
-//     `${__dirname}/data/userStorage.json`,
-//     `${__dirname}/data/chatStorage.json`
-// ),
-
-// tg = new Telegram.Telegram(process.env.TELEGRAM_TOKEN, {
-//     workers: 1,
-//     storage: storage
-// });
-
-// // const gsheet = require('./controllers/gsheet')
-
-// // console.log(new Telegram.TextCommand);
-// const AddCtrl = require('./controllers/add')
-//     , OtherwiseController = require('./controllers/otherwise');
-
-// const addcontrol = new AddCtrl();
-
-
-// // Defining Telegram commands
-// tg.router.when(new Telegram.TextCommand('/add', 'addCommand'), addcontrol)
-// .when(new Telegram.TextCommand('/total', 'totalCommand'), addcontrol)
-// .otherwise(new OtherwiseController());
-
-// function exitHandler(exitCode) {
-//     storage.flush();
-//     process.exit(exitCode);
-// }
-
-// process.on('SIGINT', exitHandler.bind(null, 0));
-// process.on('uncaughtException', exitHandler.bind(null, 1));
+process.on('SIGINT', exitHandler.bind(null, 0));
+process.on('uncaughtException', exitHandler.bind(null, 1));
