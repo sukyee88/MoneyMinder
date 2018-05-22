0479d99fac8f42aed7da01fb27d02973d0582584
ethanow <owch0002@e.ntu.edu.sg>
2018-05-22 22:17:08 +0800

    typo

--- src/telegrambot.js
+++ src/telegrambot.js
@@ -152,7 +152,7 @@ module.exports = class TelegramBot {
 
 
                         request.post(
-                            'https://api.mlab.com/api/1/databases/moneyminder_test/collections/test_env?apiKey=mlabkey',
+                            'https://api.mlab.com/api/1/databases/moneyminder_test/collections/test_env?apiKey='+mlabkey+"'",
                             {json: { 
                                 // date: Date.now(),
                                 chatID: spendlog.userID,
